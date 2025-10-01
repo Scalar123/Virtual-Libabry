170b583827a035ab0edb6138bac4ee71abfddfb6
Cauã Silva <caua.sndias@gmail.com>
2025-10-01 11:44:51 -0300

    START A VIRTUAL LIBABRY PROJECT

---
+++ src/main/java/io/desafio/Virtual/Libabry/VirtualLibabryApplication.java
@@ -0,0 +1,13 @@
+package io.desafio.Virtual.Libabry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualLibabryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualLibabryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/desafio/Virtual/Libabry/VirtualLibabryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.desafio.Virtual.Libabry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualLibabryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
